6c43c8fb58da62f29f9c01481479917581255b75
acheatham87 <acheatham87@gmail.com>
2022-05-03 08:59:12 -0500

    added a message for achievements page when nothing to display

--- src/components/goals/Achievements.js
+++ src/components/goals/Achievements.js
@@ -2,27 +2,23 @@ import React, { useEffect, useState } from "react";
 import "./Achievements.css"
 import { getCompletedGoals, deleteGoal } from "../../modules/GoalManager";
 import { AchievementCard } from "./AchievementCard";
+import { useNavigate } from "react-router-dom";
 
 export const Achievements = () => {
 
     const [goals, setGoals] = useState([])
     const currentUser = sessionStorage.getItem("level_user")
+    const navigate = useNavigate()
     
-    const getGoals = (currentUser) => {
-        return getCompletedGoals(currentUser).then(g => {
-            setGoals(g)
-        })
-    }
-
     const handleDeleteGoal = (id, currentUser) => {
         deleteGoal(id).then(() => getCompletedGoals(currentUser).then(setGoals))
     }
-    
-    useEffect(() => {
-        getGoals(currentUser);
-    }, [])
 
-    return (
+    const ifAchievements = [
+        <>
+            <h2> Your Achievments </h2>
+            <h4>It appears that you have no completed goals in your profile</h4>
+        </>,
         <>
             <div className="achievement-list">
                 <h2> Your Achievments </h2>
@@ -34,6 +30,23 @@ export const Achievements = () => {
                     )}
                 </div>
             </div>           
+        </>        
+    ]
+
+    const getGoals = (currentUser) => {
+        return getCompletedGoals(currentUser).then(g => {
+            setGoals(g)
+        })
+    }
+
+    
+    useEffect(() => {
+        getGoals(currentUser);
+    }, [])
+
+    return (
+        <>
+        {goals.length > 0 ? ifAchievements[1] : ifAchievements[0]}
         </>
     )
 }
